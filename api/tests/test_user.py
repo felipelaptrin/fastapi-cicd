9f835892398c2ec634c58e1f7fadedf02f53eb85
felipe trindade <felipelaptrin@gmail.com>
2022-05-25 18:55:01 -0300

    add tests

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -11,3 +11,35 @@ def test_get_existing_user():
 def test_get_non_existing_user():
     response = client.get("/users/-1")
     assert response.status_code == 404
+
+def test_add_user():
+    response = client.post(
+        "/users",
+        json={
+            "first_name": "John",
+            "last_name": "Doe",
+            "is_active": False
+        }
+    )
+    assert response.status_code == 201
+    assert response.json()["data"]["first_name"] == "John"
+    assert response.json()["data"]["last_name"] == "Doe"
+
+def test_delete_user():
+    response = client.delete("/users/1")
+    assert response.status_code == 200
+    assert response.json()["data"]["id"] == 1
+
+def test_update_user():
+    response = client.put(
+        "/users/2",
+        json={
+            "first_name": "Edited",
+            "last_name": "Edited",
+            "email": "edited@edited.com",
+            "is_active": True
+        }
+    )
+    assert response.status_code == 200
+    assert response.json()["data"]["first_name"] == "Edited"
+    assert response.json()["data"]["last_name"] == "Edited"
